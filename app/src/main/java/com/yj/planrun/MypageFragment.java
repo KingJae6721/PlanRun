32ba50a26013cad82d6df84d7f2b954a705fd0f6
김영재 <gotjr44@naver.com>
2023-04-07 02:58:20 +0900

    feat:menu_selector_color added (menu color) feat:frgment files need to be filled with activity_@@@@@.xml files error:calender view textColor disable we need to replace with customCalederView and test with real phone

--- app/src/main/java/com/yj/planrun/MypageFragment.java
+++ app/src/main/java/com/yj/planrun/MypageFragment.java
@@ -1,5 +1,8 @@
 package com.yj.planrun;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -9,12 +12,40 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewManager;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MypageFragment extends Fragment {
+
+    private FirebaseAuth mFirebaseAuth;
+
     @Override
     @Nullable
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
-    {
-        return inflater.inflate(R.layout.fragment_mypage, container, false);
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_mypage, container, false);
+        mFirebaseAuth = FirebaseAuth.getInstance();
+
+        Button btn_logout = (Button) view.findViewById(R.id.btn_logout);
+        btn_logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //로그아웃하기
+                mFirebaseAuth.signOut();
+                Intent intent = new Intent(getActivity(), StartActivity.class);
+                startActivity(intent);
+                SharedPreferences sharedPreferences = getActivity().getSharedPreferences("sharedPreferences", Activity.MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.clear();
+                editor.commit();
+                onDestroy();
+            }
+        });
+        //탈퇴처리
+        //mFirebaseAuth.getCurrentUser().delete();
+        return view;
     }
+
 }
+
