4337ac4ffeecee5142b4f07a1d7726741fb3a3d1
김영재 <gotjr44@naver.com>
2023-05-03 15:50:21 +0900

    지도 API연결과 화면 띄우기

--- app/src/main/java/com/yj/planrun/MainFragment.java
+++ app/src/main/java/com/yj/planrun/MainFragment.java
@@ -1,5 +1,6 @@
 package com.yj.planrun;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -9,12 +10,23 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class MainFragment extends Fragment {
+
     @Override
     @Nullable
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
-    {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+    {   View view = inflater.inflate(R.layout.fragment_main, container, false);
+
+        Button btn_run = (Button) view.findViewById(R.id.btn_run);
+        btn_run.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), MapsActivity.class);
+                startActivity(intent);
+            }
+        });
+        return view;
     }
 }
