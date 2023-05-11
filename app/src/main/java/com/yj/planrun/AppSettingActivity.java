3a06389873d176859dd219b081baf6d698cd4b48
USER <hyeokjin1018@gmail.com>
2023-05-11 22:11:56 +0900

    회원탈퇴 Authentication에서 유저 계정 삭제 추가 + 다이얼로그 메시지 추가 비밀번호 변경기능 추가 (Realtime database에서는 변경 안됨)

--- app/src/main/java/com/yj/planrun/AppSettingActivity.java
+++ app/src/main/java/com/yj/planrun/AppSettingActivity.java
@@ -1,6 +1,10 @@
 package com.yj.planrun;
 
+import static android.content.ContentValues.TAG;
+
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -25,6 +29,8 @@ public class AppSettingActivity extends AppCompatActivity {
     private FirebaseDatabase mDatabase;
     private FirebaseAuth mFirebaseAuth;         //파이어베이스 인증
     private DatabaseReference mDatabaseRef;     //실시간 데이터베이스
+    FirebaseAuth auth = FirebaseAuth.getInstance();
+    FirebaseUser user = auth.getCurrentUser();
 
 
     @Override
@@ -51,29 +57,107 @@ public class AppSettingActivity extends AppCompatActivity {
 
         });
 
+
+
+        btn_changePwd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder ad = new AlertDialog.Builder(AppSettingActivity.this);
+                ad.setIcon(R.mipmap.ic_launcher);
+                ad.setTitle("비밀번호 변경");
+                ad.setMessage("비밀번호를 변경하시겠습니까?");
+                ad.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        send();
+                        Toast.makeText(getApplicationContext(), "이메일 전송", Toast.LENGTH_SHORT).show();
+                        dialog.dismiss();
+                    }
+                });
+                ad.setNegativeButton("취소", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+                ad.show();
+            }
+        });
+
+
+
+
+
+
+
+
+
         btn_acntDel.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(AppSettingActivity.this);
+                alertDialogBuilder.setMessage("정말로 회원 탈퇴를 하시겠습니까?");
+                alertDialogBuilder.setPositiveButton("확인",
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface arg0, int arg1) {
+                                UserAccount account = new UserAccount();
 
-                UserAccount account = new UserAccount();
+                                String userToken= firebaseUser.getUid();
 
-                String userToken= firebaseUser.getUid();
+                                mDatabaseRef.child("UserAccount").child(userToken).removeValue().addOnSuccessListener(new OnSuccessListener<Void>() {
+                                    @Override
+                                    public void onSuccess(Void aVoid) {
+                                        Toast.makeText(getApplicationContext(), "회원 탈퇴가 완료되었습니다.", Toast.LENGTH_SHORT).show();
 
-                mDatabaseRef.child("UserAccount").child(userToken).removeValue().addOnSuccessListener(new OnSuccessListener<Void>() {
-                    @Override
-                    public void onSuccess(Void aVoid) {
-                        Toast.makeText(getApplicationContext(), "삭제 성공", Toast.LENGTH_SHORT).show();
-                    }
-                }).addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        System.out.println("error: "+e.getMessage());
-                        Toast.makeText(getApplicationContext(), "삭제 실패", Toast.LENGTH_SHORT).show();
-                    }
-                });
-
-             }
+                                        // Firebase Authentication에서 현재 로그인한 사용자 삭제
+                                        FirebaseAuth.getInstance().getCurrentUser().delete().addOnSuccessListener(new OnSuccessListener<Void>() {
+                                            @Override
+                                            public void onSuccess(Void aVoid) {
+                                                Intent intent = new Intent(AppSettingActivity.this, LoginActivity.class);
+                                                startActivity(intent);
+                                                finish();
+                                            }
+                                        }).addOnFailureListener(new OnFailureListener() {
+                                            @Override
+                                            public void onFailure(@NonNull Exception e) {
+                                                System.out.println("error: "+e.getMessage());
+                                                Toast.makeText(getApplicationContext(), "인증 정보 삭제 실패", Toast.LENGTH_SHORT).show();
+                                            }
+                                        });
+                                    }
+                                }).addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        System.out.println("error: "+e.getMessage());
+                                        Toast.makeText(getApplicationContext(), "회원 탈퇴에 실패하였습니다.", Toast.LENGTH_SHORT).show();
+                                    }
+                                });
+                            }
+                        });
+                alertDialogBuilder.setNegativeButton("취소",
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface arg0, int arg1) {
+                                // do nothing
+                            }
+                        });
+                AlertDialog alertDialog = alertDialogBuilder.create();
+                alertDialog.show();
+            }
         });
 
+    }
+    private void send() {
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        String emailAddress = user.getEmail();
+        mFirebaseAuth.sendPasswordResetEmail(emailAddress).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if (task.isSuccessful()) {
+                    Log.d(TAG, "Email sent.");
+                }
+            }
+        });
     }
 }
