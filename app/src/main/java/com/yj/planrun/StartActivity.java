927e165a158f4c9abb2b2790228395322d611e39
sanryon <wjdtjdfyd17@naver.com>
2023-05-17 17:45:44 +0900

    sanryon first

--- app/src/main/java/com/yj/planrun/StartActivity.java
+++ app/src/main/java/com/yj/planrun/StartActivity.java
@@ -1,15 +1,24 @@
 package com.yj.planrun;
 
+import static android.content.ContentValues.TAG;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 import com.google.firebase.auth.FirebaseAuth;
+
+import java.security.MessageDigest;
 
 public class StartActivity extends AppCompatActivity {
     @Override
@@ -28,6 +37,7 @@ public class StartActivity extends AppCompatActivity {
         }
 
         Button imageButton = (Button) findViewById(R.id.btn_login);
+        Button btn_kakao = (Button) findViewById(R.id.btn_kakao);
         imageButton.setOnClickListener(new View.OnClickListener() {
 
             @Override
@@ -36,6 +46,11 @@ public class StartActivity extends AppCompatActivity {
                 startActivity(intent1);
             }
         });
+
+        btn_kakao.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            }
+        });
     }
-    //주석
 }
