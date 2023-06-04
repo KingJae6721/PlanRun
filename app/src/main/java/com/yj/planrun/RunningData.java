c17c7bcb2a5cb35ed509c14d6995bb0c6ae80c51
김영재 <gotjr44@naver.com>
2023-06-05 03:41:54 +0900

    데이터에서 날짜 받아와서 달력에 체크표시 띄우기 err: 데이터가 업데이트 될 때  EventDecorator에 널값이 레퍼런스 되서 오류남 졸리다

---
+++ app/src/main/java/com/yj/planrun/RunningData.java
@@ -0,0 +1,46 @@
+package com.yj.planrun;
+
+public class RunningData {
+
+    private  String date;
+    private  String distance;
+    private  String calories;
+
+    public RunningData(){}
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getDistance() {
+        return distance;
+    }
+
+    public void setDistance(String distance) {
+        this.distance = distance;
+    }
+
+    public String getCalories() {
+        return calories;
+    }
+
+    public void setCalories(String calories) {
+        this.calories = calories;
+    }
+
+    public String getPace() {
+        return pace;
+    }
+
+    public void setPace(String pace) {
+        this.pace = pace;
+    }
+
+    private  String pace;
+
+
+}
