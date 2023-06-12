89c84f01fd5b695986108cd915aa8f25080577d9
김영재 <gotjr44@naver.com>
2023-06-12 23:18:59 +0900

    sanryon 병합, 닉네임 오류 해결

--- app/src/main/java/com/yj/planrun/RunRecordFragment.java
+++ app/src/main/java/com/yj/planrun/RunRecordFragment.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -59,6 +60,9 @@ public class RunRecordFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_run_record, container, false);
+        View view =inflater.inflate(R.layout.fragment_run_record2, container, false);
+        TextView tv_nickTextView=view.findViewById(R.id.nicknameTextView);
+        tv_nickTextView.setText(DataLoadingActivity.nickname);
+        return view;
     }
 }
